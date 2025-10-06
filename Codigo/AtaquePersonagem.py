29aaf6a289b162a60a10d80b9840e064e9d2c005
RyanGabriel-AZ <rgac@poli.br>
2025-10-06 19:43:50 -0300

    b

--- Codigo/AtaquePersonagem.py
+++ Codigo/AtaquePersonagem.py
@@ -3,3 +3,16 @@ Created on 5 de out. de 2025
 
 @author: albuq
 '''
+import pygame
+class AtaquePersonagem(pygame.sprite.Sprite):
+    
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load('Imagens/AtaquePersonagem.png').convert_alpha() # Carrega a imagem do ataque do personagem
+        self.rect = pygame.Rect(pos_x, pos_y, self.imagem.get_width(), self.imagem.get_height()) # Define a posição inicial do ataque do personagem
+        self.velocidade = 9 # Define a velocidade do ataque do personagem
+        
+        
+        
+    def update(self):
+        self.rect.y -= self.velocidade
--- Codigo/Laser.py
+++ Codigo/Laser.py
@@ -4,3 +4,14 @@ Created on 5 de out. de 2025
 @author: albuq
 '''
 import pygame
+class Laser(pygame.sprite.Sprite):
+    
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.image = pygame.image.load('Imagens/Laser.png').convert_alpha() # Carrega a imagem do laser
+        self.rect = pygame.Rect(pos_x, pos_y, self.image.get_width(), self.image.get_height()) # Define a posição inicial do laser
+        self.velocidade = 10 # Define a velocidade do laser
+        
+        
+    def update(self):
+        self.rect.y += self.velocidade
